fdd7bd41d7ad65b5e0b5e5cd6a931eaf93127be0
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2022-02-17 16:43:19 +0500

    Add spaces to title content

--- src/pages/Auth/Items/UpdateItem.vue
+++ src/pages/Auth/Items/UpdateItem.vue
@@ -1,16 +1,11 @@
 <template>
 	<div>
-		<h1 class="page-title">UpdateItem</h1>
+		<h1 class="page-title">Update Item</h1>
 
 		<b-row align-h="center" v-if="form">
 			<b-col cols="8">
 				<b-form @submit.prevent="submitForm">
-					<b-form-group
-						id="input-group-1"
-						label="Name:"
-						label-for="input-1"
-						class="input-form-label"
-					>
+					<b-form-group id="input-group-1" label="Name:" label-for="input-1" class="input-form-label">
 						<b-form-input
 							id="input-1"
 							v-model="form.name"
@@ -22,12 +17,7 @@
 						</span>
 					</b-form-group>
 
-					<b-form-group
-						id="input-group-2"
-						label="CBM:"
-						label-for="input-2"
-						class="input-form-label"
-					>
+					<b-form-group id="input-group-2" label="CBM:" label-for="input-2" class="input-form-label">
 						<b-form-input
 							id="input-2"
 							v-model="form.cbm"
@@ -41,12 +31,7 @@
 						</span>
 					</b-form-group>
 
-					<b-form-group
-						id="input-group-3"
-						label="Weight:"
-						label-for="input-3"
-						class="input-form-label"
-					>
+					<b-form-group id="input-group-3" label="Weight:" label-for="input-3" class="input-form-label">
 						<b-form-input
 							id="input-3"
 							v-model="form.weight"
@@ -60,12 +45,7 @@
 						</span>
 					</b-form-group>
 
-					<b-form-group
-						id="input-group-4"
-						label="Package:"
-						label-for="input-4"
-						class="input-form-label"
-					>
+					<b-form-group id="input-group-4" label="Package:" label-for="input-4" class="input-form-label">
 						<b-form-input
 							id="input-4"
 							v-model="form.package"
@@ -78,12 +58,7 @@
 						</span>
 					</b-form-group>
 
-					<b-form-group
-						id="input-group-5"
-						label="Description:"
-						label-for="input-5"
-						class="input-form-label"
-					>
+					<b-form-group id="input-group-5" label="Description:" label-for="input-5" class="input-form-label">
 						<b-form-textarea
 							id="input-5"
 							v-model="form.description"
@@ -96,25 +71,12 @@
 					</b-form-group>
 
 					<div class="add-user-btn-group">
-						<b-button
-							type="submit"
-							variant="success"
-							class="admin-add-user-add-button"
-							:disabled="loading"
-						>
-							<template v-if="loading"
-								><b-spinner small label="Small Spinner"></b-spinner
-							></template>
-							<template v-else
-								><i class="fa fa-floppy-o" aria-hidden="true"></i>
-								Update</template
-							>
+						<b-button type="submit" variant="success" class="admin-add-user-add-button" :disabled="loading">
+							<template v-if="loading"><b-spinner small label="Small Spinner"></b-spinner></template>
+							<template v-else><i class="fa fa-floppy-o" aria-hidden="true"></i> Update</template>
 						</b-button>
-						<router-link
-							to="/items"
-							class="btn btn-danger admin-add-user-cancel-button"
-							><i class="fa fa-times" aria-hidden="true"></i>
-							Cancel</router-link
+						<router-link to="/items" class="btn btn-danger admin-add-user-cancel-button"
+							><i class="fa fa-times" aria-hidden="true"></i> Cancel</router-link
 						>
 					</div>
 				</b-form>
